5e638c9ab9ad3eafcff768581964b8bb0ac01987
Harut Voskan <voskanyanharutyun@gmail.com>
2024-02-28 01:04:31 +0400

    fix circle r calc 2

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -7,4 +7,4 @@ export const PICKER_SIZE = 180;
 export const RECT_COUNT = PICKER_SIZE / RECT_SIZE;
 export const CENTER_INDEX = Math.ceil(PICKER_SIZE / 2 - RECT_SIZE / 2)
 export const CIRCLE_R = 90;
-export const CIRCLE_RECT_R = CIRCLE_R - RECT_SIZE;
+export const CIRCLE_RECT_R = CIRCLE_R - RECT_SIZE / 2;

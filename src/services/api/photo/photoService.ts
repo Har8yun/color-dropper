01f88782ce4514d83ed01ba3d1b82f30c8428e5d
Harut Voskan <voskanyanharutyun@gmail.com>
2024-03-01 12:16:08 +0400

    Add catch

--- src/services/api/photo/photoService.ts
+++ src/services/api/photo/photoService.ts
@@ -1,19 +1,29 @@
 import {PEXELS_API_KEY, PEXELS_GET, PEXELS_SEARCH} from "../constants";
 
 export const getPhotos = async () => {
-    const response = await fetch(PEXELS_GET, {
-        headers: {
-            Authorization: `${PEXELS_API_KEY}`
-        }
-    });
-    return await response.json();
+    try {
+        const response = await fetch(PEXELS_GET, {
+            headers: {
+                Authorization: `${PEXELS_API_KEY}`
+            }
+        });
+        return await response.json();
+    } catch (er) {
+        console.log("er:", er)
+        return { photos : []}
+    }
 };
 
 export const searchPhotos = async (query: string) => {
-    const response = await fetch(PEXELS_SEARCH(query), {
-        headers: {
-            Authorization: `${PEXELS_API_KEY}`
-        }
-    });
-    return await response.json();
+    try {
+        const response = await fetch(PEXELS_SEARCH(query), {
+            headers: {
+                Authorization: `${PEXELS_API_KEY}`
+            }
+        });
+        return await response.json();
+    } catch (er) {
+        console.log("er:", er)
+        return { photos : []}
+    }
 };

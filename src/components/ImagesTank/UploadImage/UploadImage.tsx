de7fa2863c4958bfc83eafe1340a8bff33477345
Harut Voskan <voskanyanharutyun@gmail.com>
2024-03-02 22:55:05 +0400

    handle image upload

--- src/components/ImagesTank/UploadImage/UploadImage.tsx
+++ src/components/ImagesTank/UploadImage/UploadImage.tsx
@@ -1,18 +1,44 @@
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import React, {useContext} from "react";
+import {ImageContext} from "../../../context/ImageContextProvider";
 
 const UploadImage = () => {
+    const { setSelectedImage } = useContext(ImageContext);
+
+    const handleImage = (ev: React.ChangeEvent<HTMLInputElement>) => {
+        const reader = new FileReader();
+
+        reader.onload = function(event: ProgressEvent<FileReader>){
+            if (event?.target?.result) {
+                setSelectedImage(event.target.result as string);
+            }
+        }
+
+        if (ev.target.files) {
+            reader.readAsDataURL(ev.target.files[0]);
+        }
+    }
+
     return (
         <Box sx={{marginBottom: "1rem"}}>
             <Button
-                onClick={() => alert("TODO: upload image")}
+                component="label"
                 fullWidth
                 color="error"
                 variant="contained"
                 startIcon={<CloudUploadIcon />}
             >
                 Upload Image
+                <input
+                    style={{display: "none"}}
+                    type="file"
+                    id="image"
+                    name="image"
+                    accept="image/png, image/jpeg"
+                    onChange={handleImage}
+                />
             </Button>
         </Box>
     );

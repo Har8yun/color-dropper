72c83ce46a05d376178657914e17daed5ed08e80
Harut Voskan <voskanyanharutyun@gmail.com>
2024-03-07 19:20:57 +0400

    Add video to cube

--- src/Webgl/webgl-demo.js
+++ src/Webgl/webgl-demo.js
@@ -1,6 +1,9 @@
 import { initBuffers } from "./init-buffers.js";
 import { drawScene } from "./draw-scene.js";
 
+// will set to true when video can be copied to texture
+let copyVideo = false;
+
 let cubeRotation = 0.0;
 let deltaTime = 0;
 //
@@ -98,17 +101,34 @@ function main(canvas) {
     const buffers = initBuffers(gl);
 
     // Load texture
-    const texture = loadTexture(gl, "logo192.png");
+    // const texture = loadTexture(gl, "logo192.png");
+    const texture = initTexture(gl);
+    const video = setupVideo("video.mp4");
+
     // Flip image pixels into the bottom-to-top order that WebGL expects.
     gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
 
     let then = 0;
 
     // Draw the scene repeatedly
+    // function render(now) {
+    //     now *= 0.001; // convert to seconds
+    //     deltaTime = now - then;
+    //     then = now;
+    //
+    //     drawScene(gl, programInfo, buffers, texture, cubeRotation);
+    //     cubeRotation += deltaTime;
+    //
+    //     requestAnimationFrame(render);
+    // }
     function render(now) {
         now *= 0.001; // convert to seconds
         deltaTime = now - then;
-        then = now;
+        then = now
+
+        if (copyVideo) {
+            updateTexture(gl, texture, video);
+        }
 
         drawScene(gl, programInfo, buffers, texture, cubeRotation);
         cubeRotation += deltaTime;
@@ -240,6 +260,102 @@ function loadTexture(gl, url) {
 
 function isPowerOf2(value) {
     return (value & (value - 1)) === 0;
+}
+
+function setupVideo(url) {
+    const video = document.createElement("video");
+
+    let playing = false;
+    let timeupdate = false;
+
+    video.playsInline = true;
+    video.muted = true;
+    video.loop = true;
+
+    // Waiting for these 2 events ensures
+    // there is data in the video
+
+    video.addEventListener(
+        "playing",
+        () => {
+            playing = true;
+            checkReady();
+        },
+        true,
+    );
+
+    video.addEventListener(
+        "timeupdate",
+        () => {
+            timeupdate = true;
+            checkReady();
+        },
+        true,
+    );
+
+    video.src = url;
+    video.play();
+
+    function checkReady() {
+        if (playing && timeupdate) {
+            copyVideo = true;
+        }
+    }
+
+    return video;
+}
+
+function initTexture(gl) {
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+
+    // Because video has to be download over the internet
+    // they might take a moment until it's ready so
+    // put a single pixel in the texture so we can
+    // use it immediately.
+    const level = 0;
+    const internalFormat = gl.RGBA;
+    const width = 1;
+    const height = 1;
+    const border = 0;
+    const srcFormat = gl.RGBA;
+    const srcType = gl.UNSIGNED_BYTE;
+    const pixel = new Uint8Array([0, 0, 255, 255]); // opaque blue
+    gl.texImage2D(
+        gl.TEXTURE_2D,
+        level,
+        internalFormat,
+        width,
+        height,
+        border,
+        srcFormat,
+        srcType,
+        pixel,
+    );
+
+    // Turn off mips and set wrapping to clamp to edge so it
+    // will work regardless of the dimensions of the video.
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+    return texture;
+}
+
+function updateTexture(gl, texture, video) {
+    const level = 0;
+    const internalFormat = gl.RGBA;
+    const srcFormat = gl.RGBA;
+    const srcType = gl.UNSIGNED_BYTE;
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(
+        gl.TEXTURE_2D,
+        level,
+        internalFormat,
+        srcFormat,
+        srcType,
+        video,
+    );
 }
 
 export { main };

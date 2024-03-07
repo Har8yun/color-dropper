d4f0839b468c9d9c496d7d8259ed3c680314cef6
Harut Voskan <voskanyanharutyun@gmail.com>
2024-03-07 18:47:49 +0400

    Image on cube

--- src/Webgl/draw-scene.js
+++ src/Webgl/draw-scene.js
@@ -1,6 +1,6 @@
 import {mat4} from "gl-matrix";
 
-function drawScene(gl, programInfo, buffers, cubeRotation) {
+function drawScene(gl, programInfo, buffers, texture, cubeRotation) {
     gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear to black, fully opaque
     gl.clearDepth(1.0); // Clear everything
     gl.enable(gl.DEPTH_TEST); // Enable depth testing
@@ -36,33 +36,33 @@ function drawScene(gl, programInfo, buffers, cubeRotation) {
     mat4.translate(
         modelViewMatrix, // destination matrix
         modelViewMatrix, // matrix to translate
-        [-0.0, 0.0, -6.0],
+        [-0.0, 0.0, -6.0]
     ); // amount to translate
 
     mat4.rotate(
         modelViewMatrix, // destination matrix
         modelViewMatrix, // matrix to rotate
         cubeRotation, // amount to rotate in radians
-        [0, 0, 1],
+        [0, 0, 1]
     ); // axis to rotate around (Z)
     mat4.rotate(
         modelViewMatrix, // destination matrix
         modelViewMatrix, // matrix to rotate
         cubeRotation * 0.7, // amount to rotate in radians
-        [0, 1, 0],
+        [0, 1, 0]
     ); // axis to rotate around (Y)
     mat4.rotate(
         modelViewMatrix, // destination matrix
         modelViewMatrix, // matrix to rotate
         cubeRotation * 0.3, // amount to rotate in radians
-        [1, 0, 0],
+        [1, 0, 0]
     ); // axis to rotate around (X)
 
     // Tell WebGL how to pull out the positions from the position
     // buffer into the vertexPosition attribute.
     setPositionAttribute(gl, buffers, programInfo);
 
-    setColorAttribute(gl, buffers, programInfo);
+    setTextureAttribute(gl, buffers, programInfo);
 
     // Tell WebGL which indices to use to index the vertices
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
@@ -74,13 +74,22 @@ function drawScene(gl, programInfo, buffers, cubeRotation) {
     gl.uniformMatrix4fv(
         programInfo.uniformLocations.projectionMatrix,
         false,
-        projectionMatrix,
+        projectionMatrix
     );
     gl.uniformMatrix4fv(
         programInfo.uniformLocations.modelViewMatrix,
         false,
-        modelViewMatrix,
+        modelViewMatrix
     );
+
+    // Tell WebGL we want to affect texture unit 0
+    gl.activeTexture(gl.TEXTURE0);
+
+    // Bind the texture to texture unit 0
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+
+    // Tell the shader we bound the texture to texture unit 0
+    gl.uniform1i(programInfo.uniformLocations.uSampler, 0);
 
     {
         const vertexCount = 36;
@@ -93,7 +102,7 @@ function drawScene(gl, programInfo, buffers, cubeRotation) {
 // Tell WebGL how to pull out the positions from the position
 // buffer into the vertexPosition attribute.
 function setPositionAttribute(gl, buffers, programInfo) {
-    const numComponents = 3; // pull out 3 values per iteration
+    const numComponents = 3;
     const type = gl.FLOAT; // the data in the buffer is 32bit floats
     const normalize = false; // don't normalize
     const stride = 0; // how many bytes to get from one set of values to the next
@@ -106,7 +115,7 @@ function setPositionAttribute(gl, buffers, programInfo) {
         type,
         normalize,
         stride,
-        offset,
+        offset
     );
     gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
 }
@@ -126,9 +135,28 @@ function setColorAttribute(gl, buffers, programInfo) {
         type,
         normalize,
         stride,
-        offset,
+        offset
     );
     gl.enableVertexAttribArray(programInfo.attribLocations.vertexColor);
 }
 
-export { drawScene };
+// tell webgl how to pull out the texture coordinates from buffer
+function setTextureAttribute(gl, buffers, programInfo) {
+    const num = 2; // every coordinate composed of 2 values
+    const type = gl.FLOAT; // the data in the buffer is 32-bit float
+    const normalize = false; // don't normalize
+    const stride = 0; // how many bytes to get from one set to the next
+    const offset = 0; // how many bytes inside the buffer to start from
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.textureCoord);
+    gl.vertexAttribPointer(
+        programInfo.attribLocations.textureCoord,
+        num,
+        type,
+        normalize,
+        stride,
+        offset
+    );
+    gl.enableVertexAttribArray(programInfo.attribLocations.textureCoord);
+}
+
+export {drawScene};
